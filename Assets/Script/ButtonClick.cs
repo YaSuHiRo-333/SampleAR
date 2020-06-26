2b9bcbfaa7299eec0464ef8b71e9b10a50c93ac6
KomureTaizo <sun.scarlet.495@ezweb.ne.jp>
2020-06-26 13:46:10 +0900

    シーンの追加

---
+++ Assets/Script/ButtonClick.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    /*
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+
+    public void OnClickTitleButton()
+    {
+        //if (SceneManager.GetActiveScene().name == "Select")
+          //  SceneManager.LoadScene("Title");
+        //else if(SceneManager.GetActiveScene().name == "AR")
+      //      SceneManager.LoadScene("Title");
+    //    else if (SceneManager.GetActiveScene().name == "Collection")
+  //          SceneManager.LoadScene("Title");
+//        else if (SceneManager.GetActiveScene().name == "Community")
+            SceneManager.LoadScene("Title");
+    }
+    
+
+    public void OnClickSelectButton()
+    {
+        SceneManager.LoadScene("Select");
+    }
+
+    public void OnClickARButton()
+    {
+        SceneManager.LoadScene("AR");
+    }
+
+    public void OnClickCollectionButton()
+    {
+        SceneManager.LoadScene("Collection");
+    }
+
+    public void OnClickCommunityButton()
+    {
+        SceneManager.LoadScene("Community");
+    }
+
+    public void OnClickMaterialButton()
+    {
+        SceneManager.LoadScene("MaterialCreate");
+    }
+
+    public void OnClickObjectButton()
+    {
+        SceneManager.LoadScene("ObjectCreate");
+    }
+
+}
